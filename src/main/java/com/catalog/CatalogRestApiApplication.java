1fe255a881981134c695e02265f26515e71aff64
Pablo Ramos <pabloramosdev@gmail.com>
2025-02-09 13:34:51 +0100

    Initial commit

---
+++ src/main/java/com/catalog/CatalogRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogRestApiApplication.class, args);
+    }
+
+}
